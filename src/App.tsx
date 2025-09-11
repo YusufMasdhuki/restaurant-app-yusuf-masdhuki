bfe58f6ee15d109aaefc928791a5b539889e2b3f
YusufMasdhuki <yusufmasduki19@gmail.com>
2025-09-11 14:41:32 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button';
+import './styles/index.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-md font-bold  text-red-500'>Welcome Back</h1>
+      <Button>Hello</Button>
+    </>
+  );
+}
+
+export default App;
